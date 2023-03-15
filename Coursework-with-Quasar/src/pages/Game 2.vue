ab75840777469b9bf604300b304ad28bd4cd5370
Sheradab Urakov <sheradaburakov@gmail.com>
2023-03-15 21:33:07 +0300

    Работа над game2

--- Coursework-with-Quasar/src/pages/Game 2.vue
+++ Coursework-with-Quasar/src/pages/Game 2.vue
@@ -1,2 +1,53 @@
 <template>
+  <div class="game2">
+   <q-page>
+    <div>
+      <div class="q-pt-xl q-mx-xl justify-center text-center">
+        <q-icon color="blue" name="chevron_left" size="50px"/>
+        <h5 class="shadow-2 q-pa-md" style="display: inline; border-radius: 30px; color:white;">Уровень 1</h5>
+        <q-icon color="blue" name="chevron_right" size="50px"/>
+      </div>
+    </div>
+    <div class="q-pt-xl justify-center row">
+      <div>
+        <img class="game211" src="../assets/game2_1.1.jpg" alt="">
+      </div>
+    </div>
+    <div class="q-pt-xl justify-center row">
+      <div>
+        <img class="game21" src="../assets/game2_1.2.jpg" alt="">
+      </div>
+      <div>
+        <img class="game21" src="../assets/game2_1.3.jpg" alt="">
+      </div>
+      <div>
+        <img class="game21" src="../assets/game2_1.4.jpg" alt="">
+      </div>
+    </div>
+  </q-page>
+  </div>
 </template>
+
+<script>
+</script>
+
+<style>
+.game2{
+  background-color: black;
+}
+
+.game211{
+  width: 500px;
+  height: 200px;
+  display: inline;
+}
+
+.game21{
+  width: 250px;
+  height: 160px;
+  display: inline;
+  margin-right:10px;
+  margin-bottom: 30px;
+}
+
+</style>
