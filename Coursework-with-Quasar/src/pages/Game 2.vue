25e4f85e599d2d7c84598f6d16871f9ac7737b83
Sheradab Urakov <sheradaburakov@gmail.com>
2023-03-16 23:58:17 +0300

    Добавлены функции в game2

--- Coursework-with-Quasar/src/pages/Game 2.vue
+++ Coursework-with-Quasar/src/pages/Game 2.vue
@@ -1,41 +1,32 @@
 <template>
-  <div class="game2">
    <q-page>
-    <div>
       <div class="q-pt-xl q-mx-xl justify-center text-center">
         <q-icon color="blue" name="chevron_left" size="50px"/>
-        <h5 class="shadow-2 q-pa-md" style="display: inline; border-radius: 30px;">Уровень 1</h5>
+        <h5 class="shadow-2 q-pa-md" style="display: inline; border-radius: 30px;" id="color">Уровень 1</h5>
         <q-icon color="blue" name="chevron_right" size="50px"/>
       </div>
-    </div>
-    <div class="q-pt-xl justify-center row">
-      <div>
+
+      <div class="q-pt-xl justify-center row">
         <img class="game211" src="../assets/game2_1.1.jpg" alt="">
       </div>
-    </div>
-    <div class="q-pt-xl justify-center row">
-      <div>
-        <img @click="wrong" class="game21" src="../assets/game2_1.2.jpg" alt="">
+
+      <div class="q-pt-xl justify-center row">
+        <img @click="colorRed" class="game21" src="../assets/game2_1.2.jpg" alt="">
+        <img @click="colorRed" class="game21" src="../assets/game2_1.3.jpg" alt="">
+        <img @click="colorGreen" class="game21" src="../assets/game2_1.4.jpg" alt="">
       </div>
-      <div>
-        <img @click="wrong" class="game21" src="../assets/game2_1.3.jpg" alt="">
-      </div>
-      <div>
-        <img @click="correct" class="game21" src="../assets/game2_1.4.jpg" alt="">
-      </div>
-    </div>
   </q-page>
-  </div>
 </template>
 
 <script>
 export default {
   methods:{
-    wrong(){
-      alert("Ты серьёзно? Попробуй ещё раз ")
+   colorRed() {
+    document.getElementById("color").style.background = "red";
     },
-    correct(){
-      alert("Так держать! Ты становишься умнее")
+    colorGreen() {
+    document.getElementById("color").style.background = "green";
+    document.getElementById("color").style.color = "white";
     }
   }
 }
@@ -59,6 +50,10 @@ export default {
 
 .game21:hover{
   transform: scale(1.05);
+}
+
+#color{
+
 }
 
 </style>
