8b6eb394bf04e382eb6f74368a2de044a5542373
Sheradab Urakov <sheradaburakov@gmail.com>
2023-03-17 00:08:23 +0300

    переключение уровней game2

--- Coursework-with-Quasar/src/pages/Game 2.vue
+++ Coursework-with-Quasar/src/pages/Game 2.vue
@@ -1,9 +1,9 @@
 <template>
    <q-page>
       <div class="q-pt-xl q-mx-xl justify-center text-center">
-        <q-icon color="blue" name="chevron_left" size="50px"/>
-        <h5 class="shadow-2 q-pa-md" style="display: inline; border-radius: 30px;" id="color">Уровень 1</h5>
-        <q-icon color="blue" name="chevron_right" size="50px"/>
+        <q-icon v-if="cur_level!==1" @click="cur_level--;Submit();" color="darkblue" name="chevron_left" size="50px"/>
+        <h5 class="shadow-2 q-pa-md" style="display: inline; border-radius: 30px;" id="color">Уровень {{ cur_level }}</h5>
+        <q-icon v-if="cur_level!==3" @click = "cur_level++;Add();" color="blue" name="chevron_right" size="50px"/>
       </div>
 
       <div class="q-pt-xl justify-center row">
@@ -15,21 +15,59 @@
         <img @click="colorRed" class="game21" src="../assets/game2_1.3.jpg" alt="">
         <img @click="colorGreen" class="game21" src="../assets/game2_1.4.jpg" alt="">
       </div>
+
+      <div v-if="loading">Loading...</div>
+      <div v-else-if="error">Error: {{ error.message }}</div>
+      <div v-else-if="result && result.game_content">
+      <div>id: {{ result.game_content[0].id }}</div>
+      <div>title: {{ result.game_content[0].image }}</div>
+    </div>
   </q-page>
 </template>
 
 <script>
-export default {
-  methods:{
-   colorRed() {
-    document.getElementById("color").style.background = "red";
-    },
-    colorGreen() {
-    document.getElementById("color").style.background = "green";
-    document.getElementById("color").style.color = "white";
+import { defineComponent,ref } from 'vue'
+import { useQuery} from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+export default defineComponent({
+  setup () {
+    const cur_level = ref(1)
+    const { result, error, loading, refetch } = useQuery(gql`query my($levels: Int!) {
+      game_content(where: {levels: {_eq:$levels}}) {
+      levels
+      image
+      game_name
+      id
+      }
+    }
+    `,{"levels":cur_level})
+    console.log(result);
+    console.log(error)
+    return{
+      cur_level,
+      result,
+      loading,
+      error,
+      refetch,
+      Add(){
+        refetch()
+        console.log(result);
+      },
+      Submit(){
+        refetch()
+        console.log(result);
+      },
+      colorRed() {
+        document.getElementById("color").style.background = "red";
+      },
+      colorGreen() {
+        document.getElementById("color").style.background = "green";
+        document.getElementById("color").style.color = "white";
+      }
     }
   }
-}
+})
 </script>
 
 <style>
