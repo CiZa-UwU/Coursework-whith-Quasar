8f21d410e2f900fb93634cd654d0fd5be32ff031
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2023-03-16 18:14:44 +0300

    Merge

---
+++ Coursework-with-Quasar/dist/spa/js/393.1f7d071c.js
@@ -0,0 +1,8 @@
+"use strict";(globalThis["webpackChunkcoursework_whith_quasar"]=globalThis["webpackChunkcoursework_whith_quasar"]||[]).push([[393],{393:(e,r,s)=>{s.r(r),s.d(r,{default:()=>p});var a=s(9835);const n=(0,a._)("div",{class:"greetings_title"},[(0,a._)("p",null,"Proкачай свой мозг с нами.")],-1),t=(0,a._)("div",{class:"brain"},[(0,a._)("img",{class:"brain_img",src:"https://i.gifer.com/origin/86/867536d1390d47a35f0c0e29ed9aef0f.gif",alt:""})],-1);function i(e,r,s,i,o,c){const g=(0,a.up)("q-page");return(0,a.wg)(),(0,a.j4)(g,{class:"greetings"},{default:(0,a.w5)((()=>[n,t,(0,a.Uk)("> ")])),_:1})}var o=s(4886),c=s(8010);const g=(0,a.aZ)({name:"Greetings",setup(){const{result:e,loading:r,error:s}=(0,o.aM)(c.ZP`
+        query MyQyery {
+          game_content{
+            id
+            image
+          }
+        }
+      `);return console.log(e),{result:e,loading:r,error:s}}});var l=s(1639),u=s(9885),d=s(9984),_=s.n(d);const h=(0,l.Z)(g,[["render",i]]),p=h;_()(g,"components",{QPage:u.Z})}}]);
