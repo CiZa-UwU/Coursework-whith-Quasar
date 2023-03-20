04074d566ce0ca1c64f203ec347450160bc5135a
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2023-03-20 11:23:16 +0300

    Добавлены чекбоксы и начата работа со стором

--- Coursework-with-Quasar/src/components/NavList.vue
+++ Coursework-with-Quasar/src/components/NavList.vue
@@ -7,15 +7,24 @@
       <q-card class="my-card q-pa-md q-ma-md">
       <img src="https://media.discordapp.net/attachments/1084917962487631913/1085265393574236200/the_apple_divided_into_two_parts_one_part_of_the_ap_f213ffcc-4c26-422d-b5b1-7ec2161f36a8.png?width=738&height=422">
       <q-list class="list_background">
-        <q-item clickable>
-          <q-item-section avatar>
+        <q-item clickable >
+          <q-item-section avatar> 
             <q-icon color="primary" name="keyboard_arrow_right" />
           </q-item-section>
           <q-item-section>
             <router-link to="game-1" class="link">
-                <q-item-label>Соедини половинки</q-item-label>
+                <q-item-label>Соедини половинки
+                </q-item-label>
                 <q-item-label caption>Перейти</q-item-label>
             </router-link>
+          </q-item-section>
+          <q-item-section avatar>
+            <q-radio v-model="shape1" checked-icon="task_alt" 
+                  unchecked-icon="panorama_fish_eye" 
+                  val="line" 
+                  color="green"
+                  style="width: 10px; height: 10px;"
+            />
           </q-item-section>
         </q-item>
       </q-list>
@@ -30,9 +39,18 @@
           </q-item-section>
           <q-item-section>
             <router-link to="game-2" class="link">
-                <q-item-label>Найди правильный ответ</q-item-label>
+                <q-item-label>Найди правильный ответ
+                </q-item-label>
                 <q-item-label caption>Перейти</q-item-label>
             </router-link>
+          </q-item-section>
+          <q-item-section avatar>
+            <q-radio v-model="shape1" checked-icon="task_alt" 
+                  unchecked-icon="panorama_fish_eye" 
+                  val="line" 
+                  color="green"
+                  style="width: 10px; height: 10px;"
+            />
           </q-item-section>
         </q-item>
       </q-list>
@@ -47,9 +65,18 @@
           </q-item-section>
           <q-item-section>
             <router-link to="game-3" class="link">
-                <q-item-label>Игра на запоминание</q-item-label>
+                <q-item-label>Игра на запоминание
+                </q-item-label>
                 <q-item-label caption>Перейти</q-item-label>
             </router-link>
+          </q-item-section>
+          <q-item-section avatar>
+            <q-radio v-model="shape1" checked-icon="task_alt" 
+                  unchecked-icon="panorama_fish_eye" 
+                  val="line" 
+                  color="green"
+                  style="width: 10px; height: 10px;"
+            />
           </q-item-section>
         </q-item>
       </q-list>
@@ -58,12 +85,19 @@
     </q-list>
 </template>
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
     name: 'NavList',
     setup() {
-
+      const shape1 = ref('line')
+      const shape2 = ref('line')
+      const shape3 = ref('line')
+      return{
+        shape1,
+        shape2,
+        shape3
+      }
     },
 })
 </script>
