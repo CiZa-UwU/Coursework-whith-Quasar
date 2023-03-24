04a606cbd37852a596385292bc27ab9217c514a7
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2023-03-24 10:23:06 +0300

    Отправлено на хостинг

--- Coursework-with-Quasar/src/router/routes.js
+++ Coursework-with-Quasar/src/router/routes.js
@@ -8,9 +8,6 @@ const routes = [
       { path: '/game-1', component: () => import('pages/Game 1.vue') },
       { path: '/game-2', component: () => import('pages/Game 2.vue') },
       { path: '/game-3', component: () => import('pages/Game 3.vue') },
-      { path: '/about', component: () => import('pages/About.vue') },
-      { path: '/enter', component: () => import('pages/Enter.vue') },
-      { path: '/register', component: () => import('pages/Register.vue') },
       { path: '/chat', component: () => import('pages/Chat.vue') },
     ]
   },
