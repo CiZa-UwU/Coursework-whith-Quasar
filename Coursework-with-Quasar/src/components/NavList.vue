7f8514a7287c83c4e69902404f592254e582121e
chernyshov_oleg <oleg.chernyshov.960@mail.ru>
2023-06-11 11:38:16 +0300

    Исправил баг

--- Coursework-with-Quasar/src/components/NavList.vue
+++ Coursework-with-Quasar/src/components/NavList.vue
@@ -1,10 +1,12 @@
 <template>
-    <q-list>
-      <q-item-label class="list_header" header>
-        <q-img src="../assets/game-logo.png" class="center"></q-img>
-      </q-item-label>
+  <q-list>
+    <q-item-label class="list_header" header>
+      <q-img src="../assets/game-logo.png" class="center"></q-img>
+    </q-item-label>
     <q-card class="my-card q-pa-md q-ma-md">
-      <img src="https://media.discordapp.net/attachments/1084917962487631913/1085265393574236200/the_apple_divided_into_two_parts_one_part_of_the_ap_f213ffcc-4c26-422d-b5b1-7ec2161f36a8.png?width=738&height=422">
+      <img
+        src="https://media.discordapp.net/attachments/1084917962487631913/1085265393574236200/the_apple_divided_into_two_parts_one_part_of_the_ap_f213ffcc-4c26-422d-b5b1-7ec2161f36a8.png?width=738&height=422"
+      />
       <q-list class="list_background">
         <q-item clickable>
           <q-item-section avatar>
@@ -12,15 +14,17 @@
           </q-item-section>
           <q-item-section>
             <router-link to="game-1" class="link">
-                <q-item-label>Соедини половинки</q-item-label>
-                <q-item-label caption>Перейти</q-item-label>
+              <q-item-label>Соедини половинки</q-item-label>
+              <q-item-label caption>Перейти</q-item-label>
             </router-link>
           </q-item-section>
         </q-item>
       </q-list>
     </q-card>
     <q-card class="my-card q-pa-md q-ma-md">
-      <img src="https://media.discordapp.net/attachments/1084917962487631913/1084921429163769856/sfsfadfadf_background_for_header_in_site_puzzles_games_and_brai_72671b22-88ce-4dc8-a89b-e7247e292af7.png?width=738&height=422">
+      <img
+        src="https://media.discordapp.net/attachments/1084917962487631913/1084921429163769856/sfsfadfadf_background_for_header_in_site_puzzles_games_and_brai_72671b22-88ce-4dc8-a89b-e7247e292af7.png?width=738&height=422"
+      />
       <q-list class="list_background">
         <q-item clickable>
           <q-item-section avatar>
@@ -28,9 +32,8 @@
           </q-item-section>
           <q-item-section>
             <router-link to="game-2" class="link">
-                <q-item-label>Найди правильный ответ
-                </q-item-label>
-                <q-item-label caption>Перейти</q-item-label>
+              <q-item-label>Найди правильный ответ </q-item-label>
+              <q-item-label caption>Перейти</q-item-label>
             </router-link>
           </q-item-section>
         </q-item>
@@ -38,7 +41,7 @@
     </q-card>
 
     <q-card class="my-card q-pa-md q-ma-md">
-      <img src="https://media.discordapp.net/ephemeral-attachments/1084917962487631913/1087013080518639696/grid_0.webp?width=732&height=422">
+      <img src="../../public/mzl.dcnifpch.png" class="img" />
       <q-list class="list_background">
         <q-item clickable>
           <q-item-section avatar>
@@ -46,44 +49,42 @@
           </q-item-section>
           <q-item-section>
             <router-link to="game-3" class="link">
-                <q-item-label>Игра на запоминание
-                </q-item-label>
-                <q-item-label caption>Перейти</q-item-label>
+              <q-item-label>Игра на запоминание </q-item-label>
+              <q-item-label caption>Перейти</q-item-label>
             </router-link>
           </q-item-section>
         </q-item>
       </q-list>
     </q-card>
-
-    </q-list>
+  </q-list>
 </template>
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-    name: 'NavList',
-    setup() {
-      const shape1 = ref('line')
-      const shape2 = ref('line')
-      const shape3 = ref('line')
-      return{
-        shape1,
-        shape2,
-        shape3
-      }
-    },
-})
+  name: "NavList",
+  setup() {
+    const shape1 = ref("line");
+    const shape2 = ref("line");
+    const shape3 = ref("line");
+    return {
+      shape1,
+      shape2,
+      shape3,
+    };
+  },
+});
 </script>
 <style>
-::-webkit-scrollbar{
+::-webkit-scrollbar {
   width: 5px;
 }
 
-::-webkit-scrollbar-track{
+::-webkit-scrollbar-track {
   background-color: black;
 }
 
-::-webkit-scrollbar-thumb{
+::-webkit-scrollbar-thumb {
   background-color: #000000;
   border-radius: 8px;
 }
@@ -93,7 +94,9 @@ export default defineComponent({
   background-size: cover;
   background-position: center;
 }
-
+.img {
+  height: 180px;
+}
 .list_header {
   background: #079270;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
--- Coursework-with-Quasar/src/pages/Game 1.vue
+++ Coursework-with-Quasar/src/pages/Game 1.vue
@@ -1,106 +1,158 @@
 <template>
   <q-page class="main-wrapper">
-  <!-- Игры работают по принципу получения пути к картинке из бд и установкой класса answer на правильную -->
-  <!-- Проверка работает по принципу получения данных пользователя и поиска в них пройдена ли игра -->
-  <div class="mini-wrapper q-mb-lg q-pb-lg" :class="{'done' : levelDone}">
-  <div class="q-pt-xl q-mx-xl justify-center text-center">
-      <q-icon v-if="cur_level!==1" 
-      @click="cur_level--;
-              refetch(); //Отправляет запрос заново с новыми данными
-              res=[];
-              " 
-      color="black" 
-      name="chevron_left" 
-      size="50px"
-      />
-      <h5 class="shadow-2 q-pa-md level-bar">
-        Уровень {{ cur_level }}
-      </h5>
-      <q-icon v-if="cur_level!==2" 
-        @click ="cur_level++;
-                refetch(); //Отправляет запрос заново с новыми данными
-                res=[];
-                " 
-        color="black" 
-        name="chevron_right" 
-        size="50px"
-      />
-    </div>
-    <h6 v-if="levelDone" class="row justify-center">Вы уже прошли этот уровень, поздравляем!!!</h6>
-    <div class="q-pt-xl justify-center row">
-      <img :src="require('../assets/game1/'+cur_level+'level-answer.png')"
-           :class="{'card_level_1' : cur_level == 1, 'card_level_2' : cur_level == 2}"
-           >
+    <!-- Игры работают по принципу получения пути к картинке из бд и установкой класса answer на правильную -->
+    <!-- Проверка работает по принципу получения данных пользователя и поиска в них пройдена ли игра -->
+    <div class="mini-wrapper q-mb-lg q-pb-lg" :class="{ done: levelDone }">
+      <div class="q-pt-xl q-mx-xl justify-center text-center">
+        <q-icon
+          v-if="cur_level !== 1"
+          @click="
+            cur_level--;
+            refetch(); //Отправляет запрос заново с новыми данными
+            res = [];
+          "
+          color="black"
+          name="chevron_left"
+          size="50px"
+        />
+        <h5 class="shadow-2 q-pa-md level-bar">Уровень {{ cur_level }}</h5>
+        <q-icon
+          v-if="cur_level !== 2"
+          @click="
+            cur_level++;
+            refetch(); //Отправляет запрос заново с новыми данными
+            res = [];
+          "
+          color="black"
+          name="chevron_right"
+          size="50px"
+        />
+      </div>
+      <h6 v-if="levelDone" class="row justify-center">
+        Вы уже прошли этот уровень, поздравляем!!!
+      </h6>
+      <div class="q-pt-xl justify-center row">
+        <img
+          :src="require('../assets/game1/' + cur_level + 'level-answer.png')"
+          :class="{
+            card_level_1: cur_level == 1,
+            card_level_2: cur_level == 2,
+          }"
+        />
         <!-- Между групп с одинаковым названием можно перетаскивать. 
       pull - действие при перетаскивании put - возможность претащить list - массив с данными 
       template - выводит в цикле картинки из слота item @add - событие при добавлении -->
-      <draggable
-        :list="res" 
-        group = "a" 
-        @add="Check"
-        class="card"
-        :class="{'card_level_1' : cur_level == 1, 'card_level_2' : cur_level == 2}"
-        item-key="res.image">
-        <template #item = "{ element }" >
-          <div class="list-group-item">
-            <img class="q-mx-md" 
-              :src="require('../assets/game1/'+element.image)" 
-              :class="{'card_level_1' : cur_level == 1, 'card_level_2' : cur_level == 2}"
+        <draggable
+          :list="res"
+          group="a"
+          @add="Check"
+          class="card"
+          :class="{
+            card_level_1: cur_level == 1,
+            card_level_2: cur_level == 2,
+          }"
+          item-key="res.image"
+        >
+          <template #item="{ element }">
+            <div class="list-group-item">
+              <img
+                class="q-mx-md"
+                :src="require('../assets/game1/' + element.image)"
+                :class="{
+                  card_level_1: cur_level == 1,
+                  card_level_2: cur_level == 2,
+                }"
+                alt="img"
+              />
+            </div>
+          </template>
+        </draggable>
+      </div>
+      <div class="q-pt-xl justify-center row">
+        <div v-if="loading || loading2">
+          <div class="brain">
+            <img class="brain_img" src="../assets/mozg.gif" alt="" />
+          </div>
+        </div>
+        <!-- @vnode-mounted сработает когда появится элемент  -->
+        <draggable
+          v-else
+          @vnode-mounted="CheckLevel()"
+          :list="result.game_content"
+          :sort="false"
+          :group="{ name: 'a', pull: 'clone', put: 'false' }"
+          :disabled="levelDone"
+          item-key="result.game_content.id"
+        >
+          <template #item="{ element }">
+            <q-img
+              class="q-mx-md col-4"
+              :class="{
+                answer: element.is_answer,
+                card_level_1: cur_level == 1,
+                card_level_2: cur_level == 2,
+              }"
+              :src="require('../assets/game1/' + element.image)"
               alt="img"
             />
-          </div>
-        </template>
-      </draggable>
-    </div>
-    <div class="q-pt-xl justify-center row">
-      <div v-if="loading || loading2">
-        <div class="brain">
-            <img class="brain_img"
-              src="../assets/mozg.gif" alt="">
-        </div>
+          </template>
+        </draggable>
       </div>
-      <!-- @vnode-mounted сработает когда появится элемент  -->
-      <draggable v-else
-        @vnode-mounted="CheckLevel()"
-        :list="result.game_content"
-        :sort="false" 
-        :group="{name:'a',pull:'clone',put:'false'}"  
-        :disabled="levelDone" 
-        item-key="result.game_content.id"
-        >
-        <template #item="{ element }">
-          <q-img class="q-mx-md col-4" 
-            :class="{'answer' : element.is_answer, 'card_level_1' : cur_level == 1, 'card_level_2' : cur_level == 2}" 
-            :src="require('../assets/game1/'+element.image)" 
-            alt="img"
-          />
-        </template>
-      </draggable>
     </div>
-    </div>
-    
   </q-page>
 </template>
 
 <script>
-import { defineComponent,ref,computed } from 'vue'
-import { useQuasar } from 'quasar';
-import { useQuery, useMutation } from '@vue/apollo-composable'
+import { defineComponent, ref, computed, watch } from "vue";
+import { useQuasar } from "quasar";
+import { useQuery, useMutation } from "@vue/apollo-composable";
 import draggable from "vuedraggable";
-import { GetLevelData, GetUserData, AddDoneLevel } from "../apollo/query/queryes.js"
+import {
+  GetLevelData,
+  GetUserData,
+  AddDoneLevel,
+} from "../apollo/query/queryes.js";
 
 export default defineComponent({
-  name: 'Game 1',
-  components: {draggable},
-  setup () {
-    const $q = useQuasar() // Для notify
-    const cur_level = ref(1) // Текущий уровень
-    const {result, loading, error, refetch} = useQuery(computed( () => GetLevelData ), {"levels":cur_level})
-    const {result:result2,loading:loading2} = useQuery(computed( () => GetUserData))
-    const { mutate : mutate2 } = useMutation(AddDoneLevel)
-    const levelDone = ref(0)
-    const res = ref([])
-    return{
+  name: "Game 1",
+  components: { draggable },
+  setup() {
+    const $q = useQuasar(); // Для notify
+    const cur_level = ref(1); // Текущий уровень
+    const { result, loading, error, refetch } = useQuery(
+      computed(() => GetLevelData),
+      { levels: cur_level }
+    );
+    const {
+      result: result2,
+      loading: loading2,
+      refetch: refetch2,
+    } = useQuery(computed(() => GetUserData));
+
+    const { mutate: mutate2 } = useMutation(AddDoneLevel);
+    const levelDone = ref(0);
+    const res = ref([]);
+    const CheckLevel = () => {
+      //Будет вызываться когда все данные уже получены
+      if (window.Clerk.user) {
+        let flag = 0;
+        result2._rawValue.done_levels.forEach((element) => {
+          if (element.level == cur_level.value && element.done == true) {
+            flag = 1;
+            levelDone.value = 1;
+          } else if (flag != 1) {
+            levelDone.value = 0;
+          }
+        });
+      } else {
+        levelDone.value = 0;
+      }
+    };
+    watch(cur_level, () => {
+      refetch2();
+      CheckLevel();
+    });
+    return {
       result2,
       loading2,
       cur_level,
@@ -111,58 +163,51 @@ export default defineComponent({
       res,
       levelDone,
       //Methods
-      Check(env){ //Будет вызываться когда добавляет новый элемент
-        if(env.item.__draggable_context.element.is_answer && res.value.length == 1){
+      Check(env) {
+        //Будет вызываться когда добавляет новый элемент
+        if (
+          env.item.__draggable_context.element.is_answer &&
+          res.value.length == 1
+        ) {
           $q.notify({
-            message: 'Правильный ответ!',
-            color: 'green'
-          })
+            message: "Правильный ответ!",
+            color: "green",
+          });
           levelDone.value = 1;
-          if(window.Clerk.user){
-            mutate2({"game":1,"level":cur_level.value})
+          if (window.Clerk.user) {
+            mutate2({ game: 1, level: cur_level.value });
           }
-        }
-        else{
-          res.value = []
+        } else {
+          res.value = [];
           $q.notify({
-            message: 'Попробуй еще раз!',
-            color: 'red'
-          })
+            message: "Попробуй еще раз!",
+            color: "red",
+          });
         }
       },
-      CheckLevel(){ //Будет вызываться когда все данные уже получены
-        if(window.Clerk.user){
-        result2._rawValue.done_levels.forEach(element => {
-          if(element.level == cur_level.value && element.game == 1 && element.done == true){
-            levelDone.value = 1;
-          }
-        });
-
-      }
-      }
-    }
-  }
-})
+      CheckLevel,
+    };
+  },
+});
 </script>
 <style scoped>
-
-.level-bar{
+.level-bar {
   display: inline;
   border-radius: 35px;
-  background: #00684F;
+  background: #00684f;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
   color: white;
-  }
+}
 
-.card{
+.card {
   width: 100px;
   height: 100px;
 }
-.card_level_1{
+.card_level_1 {
   width: 100px;
 }
-.card_level_2{
+.card_level_2 {
   height: 150px;
   width: 175px;
 }
@@ -171,7 +216,7 @@ export default defineComponent({
   background: white;
   border-radius: 50px;
 }
-.done{
+.done {
   background-color: lightgreen;
 }
 .main-wrapper {
